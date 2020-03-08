06ab53dee15503dae7ed64d173d11323747ececf
Viktor <tass99@mail.ru>
2020-03-08 15:30:32 +0300

    Добавлен вывод очков на экран

--- Clicker/Assets/Scripts/Click.cs
+++ Clicker/Assets/Scripts/Click.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Click : MonoBehaviour
 {
-    public int score = 0;
+   
+    public Text scoreText;
+    private int score = 0;
 
     public void OnClick()
     {
         score++;   
+        scoreText.text = score + '$';
     }
 
     
